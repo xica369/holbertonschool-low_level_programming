81da4d646ccce990e450d23f46c36d16f1b6ac9c
xica369 <761@holbertonschool.com>
2019-02-06 22:54:43 +0000

    exercise 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main - Print a string
+*
+*Description: Print: Programming is like building a multilingual puzzle
+*
+*Return: return 0
+*/
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
