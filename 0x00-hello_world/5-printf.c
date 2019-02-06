862970a8ba78c7d5460fa4d92130d9a4f3005617
xica369 <761@holbertonschool.com>
2019-02-06 23:22:35 +0000

    Exercise 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main - printf
+*
+*Description: Print a string
+*
+*Return: returns 0
+*/
+void main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
