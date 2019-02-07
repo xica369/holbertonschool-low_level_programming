2b9c7f7c4ec121b891089b3ce83c6e37c260ee47
xica369 <761@holbertonschool.com>
2019-02-07 21:50:07 +0000

    exercise 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - print the alphabet
+ *
+ *Return: zero
+ */
+
+int main(void)
+{
+char n;
+for (n = 'a'; n < 'z'; n++)
+{
+putchar (n);
+}
+putchar ('\n');
+return (0);
+}
