b8826c9296b07ab99adb2024c733a39844a6ef7e
xica369 <761@holbertonschool.com>
2019-02-08 00:15:29 +0000

    Exercise 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - print the numbers 0-9
+ *
+ *Return: zero
+ */
+int main(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
