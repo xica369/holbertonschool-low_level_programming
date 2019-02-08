f374e94465032599ea8b31d7f6ca3e59fd4cda79
xica369 <761@holbertonschool.com>
2019-02-08 00:30:38 +0000

    Exercise 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - Print the alphafet reverse
+ *
+ *Return: zero
+ */
+int main(void)
+{
+char n;
+for (n = 'z'; n >= 'a'; n--)
+{
+putchar(n);
+}
+putchar ('\n');
+return (0);
+}
