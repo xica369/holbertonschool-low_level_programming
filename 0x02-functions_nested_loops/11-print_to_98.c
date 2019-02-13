2f28ab422af2cef5c8fb879cc7a5f1b9573f28d1
xica369 <761@holbertonschool.com>
2019-02-13 14:00:48 +0000

    exercise 11 again 3

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,7 @@ void print_to_98(int n)
 
 	if (n > 98)
 	{
-		for (x = n; x > 98; x--)
+		for (x = n; x >= 98; x--)
 		{
 			printf("%d", x);
 			if (x != 98)
@@ -28,7 +28,7 @@ void print_to_98(int n)
 	}
 	if (n < 98)
 	{
-		for (x = n; x < 98; x++)
+		for (x = n; x <= 98; x++)
 		{
 			printf("%d", x);
 		if (x != 98)
