3b889dafc4200ea804fa8bb5158011f127cac23f
xica369 <761@holbertonschool.com>
2019-02-14 19:29:11 +0000

    checks for a digit (0 through 9)

---
+++ 0x03-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *isdigit - checks for a digit (0 through 9)
+ *
+ *@c: character to evaluate if it is digit
+ *
+ *Return: one if the character is a digit else return zero
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '0')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
