0b5c9837e3c9cd0edfa100874392933f904a9faa
xica369 <761@holbertonschool.com>
2019-02-16 23:25:32 +0000

    largest prime factor of the number 612852475143

---
+++ 0x03-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main - prints the largest prime factor of the number 612852475143
+ *
+ *Return: zero
+ */
+int main(void)
+{
+	long int x, b;
+
+	b = 612852475143;
+	for (x = 2; x <= b; x++)
+	{
+		if (b % x == 0)
+		{
+			b = b / x;
+		}
+		if (b == 1)
+		{
+			printf("%ld\n", x);
+			break;
+		}
+	}
+	return (0);
+}
