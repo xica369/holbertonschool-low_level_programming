4ec200ede5fbf01b9c0d0d56f0a317ea14dcd6c3
xica369 <761@holbertonschool.com>
2019-02-21 22:54:23 +0000

    concatenates two strings until n, again

--- 0x05-pointers_arrays_strings/1-strncat.c
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -9,14 +9,21 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int cont1, cont2;
+	int cont1, cont2, cont3;
 
 	for (cont1 = 0; dest[cont1]; cont1++)
 	{
 	}
-	for (cont2 = 0; cont2 < n; cont2++)
+	for (cont2 = 0; dest[cont2]; cont2++)
 	{
-		dest[cont1 + cont2] = src[cont2];
+	}
+	if (n > cont2)
+	{
+		n = cont2;
+	}
+	for (cont3 = 0; cont3 < n; cont3++)
+	{
+		dest[cont1 + cont3] = src[cont3];
 	}
 	/*dest[cont1 + cont2] = 0;*/
 	return (dest);
