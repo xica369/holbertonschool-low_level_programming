49c8ad40abcfc81f1b81465ea7664216929a2fa3
xica369 <761@holbertonschool.com>
2019-02-22 00:23:41 +0000

    copies a string since first posicion to n, again

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -10,11 +10,21 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int cont1;
+	int cont1, cont2;
 
+	for (cont2 = 0; src[cont2]; cont2++)
+	{
+	}
 	for (cont1 = 0; cont1 < n; cont1++)
 	{
+		if (cont1 < cont2)
+		{
 		dest[cont1] = src[cont1];
+		}
+		else
+		{
+			dest[cont1] = 0;
+		}
 	}
 	return (dest);
 }
