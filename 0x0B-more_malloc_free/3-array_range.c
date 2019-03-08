fe7df453692ab645aceda18b4973b46812cb2fbd
xica369 <761@holbertonschool.com>
2019-03-08 02:22:35 +0000

    function that creates an array of integers, max - min

--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -11,22 +11,21 @@
  */
 int *array_range(int min, int max)
 {
-	int size, cont;
+	int cont, i = 0;
 	int *s;
 
 	if (min > max)
 	{
 		return (NULL);
 	}
-	size = max - min;
-	s = malloc(sizeof(*s) * (size + 2));
+	s = malloc(sizeof(*s) * (max - min + 1));
 	if (s == NULL)
 	{
 		return (NULL);
 	}
-	for (cont = min; cont <= max; cont++)
+	for (cont = min; cont <= max; cont++, i++)
 	{
-		s[cont] = cont;
+		s[i] = cont;
 	}
 	return (s);
 }
