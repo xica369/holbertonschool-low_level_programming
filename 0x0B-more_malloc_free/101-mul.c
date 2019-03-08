c2bd909e2343645336af7dd27c423ef5eb74e41e
xica369 <761@holbertonschool.com>
2019-03-08 03:46:16 +0000

    program that multiplies two positive numbers

---
+++ 0x0B-more_malloc_free/101-mul.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *main - program that multiplies two positive numbers
+ *
+ *@argv: array with arguments
+ *@argc: number to arguments
+ *
+ *Return: multiplies
+ */
+void main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+}
