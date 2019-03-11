fdd963efe81e4c4c3dafcd1971dc365cf978a9f2
xica369 <761@holbertonschool.com>
2019-03-11 23:43:49 +0000

    struct dog with: name, age and owner

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#ifndef _DOG_
+#define _DOG_
+/**
+ *struct dog -  has information over the dog
+ *@name: name of dog
+ *@owner: owner of dog
+ *@age: age of dog
+ */
+struct dog
+{
+	char *name, *owner;
+	float age;
+};
+#endif
