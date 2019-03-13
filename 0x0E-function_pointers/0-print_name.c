1d757296dcafd08dc85b13753fd6cf2ae69db09b
xica369 <761@holbertonschool.com>
2019-03-13 18:51:13 +0000

    function that prints a name

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ *print_name - function that prints a name
+ *
+ *@name: pointer to name to print
+ *@f: pointer to array of function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
