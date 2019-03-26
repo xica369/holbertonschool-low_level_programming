3009adb8818dcd47c3c38eff1ff2edea8aa32ff8
xica369 <761@holbertonschool.com>
2019-03-26 21:01:54 +0000

    T6: Write a function that deletes the head node of a listint_t linked list, and returns the head nodes data (n), with Betty

--- 0x12-more_singly_linked_lists/6-pop_listint.c
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -14,10 +14,10 @@ int pop_listint(listint_t **head)
 	listint_t *temp;
 	int n;
 
-        if (*head == NULL)
-        {
-                return (0);
-        }
+	if (*head == NULL)
+	{
+		return (0);
+	}
 	else
 	{
 		temp = *head;
