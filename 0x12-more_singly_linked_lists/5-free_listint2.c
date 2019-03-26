0c86f4530cda403f10cdfede06cdc9b805c3327b
xica369 <761@holbertonschool.com>
2019-03-26 21:29:11 +0000

    T5: function that frees a listint_t list and put head = null

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -11,7 +11,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *nodo;
 
-	if (*head != NULL)
+	if (head != NULL)
 	{
 		nodo = *head;
 		while (nodo != NULL)
@@ -19,6 +19,6 @@ void free_listint2(listint_t **head)
 			free(nodo);
 			nodo = nodo->next;
 		}
+		*head = NULL;
 	}
-	*head = NULL;
 }
