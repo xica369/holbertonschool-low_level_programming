c05031fb1a461bf8b78f3311905dd35f3dd1687f
xica369 <761@holbertonschool.com>
2019-04-01 02:57:46 +0000

    T6: function that returns 1 if the input integer is a prime number, otherwise return 0

---
+++ 0x07-recursion/6-is_prime_number.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+int prime(int n, int x);
+/**
+ *prime - say if a number is prime or no
+ *
+ *@n: number to evalue
+ *@x: aux to evalue the number
+ *
+ *Return: 1 if the number is prime else 0
+ */
+int prime(int n, int x)
+{
+
+	if (n % x == 0)
+	{
+		return (0);
+	}
+	if (n / x != 0 && x < n / 2)
+	{
+		return (prime(n, x + 1));
+	}
+	else
+		return (1);
+}
+/**
+ *is_prime_number - say if a number is prime or no
+ *
+ *@n: number to evalue
+ *
+ *Return: 1 if the number is prime else 0
+ */
+int is_prime_number(int n)
+{
+	int x = 2;
+
+	if (n == 1 || n == -1)
+	{
+		return (0);
+	}
+	else
+	{
+		return (prime(n, x));
+	}
+}
