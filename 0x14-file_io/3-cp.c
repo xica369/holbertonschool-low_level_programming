bf133ae21daf65f151c98de724005fdb570995d6
xica369 <761@holbertonschool.com>
2019-04-04 20:46:05 +0000

    T3- program that copies the content of a file to another file, change loop

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -19,26 +19,24 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
 		exit(97);
 	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
-		dprintf(STDERR_FILENO,
-			"Error: Can't read from file %s\n", argv[1]),
-			exit(98);
 	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd2 == -1)
-		dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", argv[2]),
-			exit(99);
-	while (aux)
-	{n = read(fd, bf, 1024);
+	n = read(fd, bf, 1024);
 	if (n == -1)
 		dprintf(STDERR_FILENO,
 			"Error: Can't read from file %s\n", argv[1]),
 			exit(98);
+	while (aux)
+	{
 	aux = write(fd2, bf, n);
 	if (aux == -1)
 		dprintf(STDERR_FILENO,
 			"Error: Can't write to %s\n", argv[2]),
 			exit(99);
+	n = read(fd, bf, 1024);
+	if (n == -1)
+		dprintf(STDERR_FILENO,
+			"Error: Can't read from file %s\n", argv[1]),
+			exit(98);
 	}
 	aux = close(fd);
 	if (aux == -1)
