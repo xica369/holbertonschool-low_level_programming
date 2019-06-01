a30f1f08598871e57a6774e79581cc61be7786f4
xica369 <761@holbertonschool.com>
2019-06-01 02:36:16 +0000

    T3: function that adds an element to the hash table, again 2

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -37,7 +37,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		new->key = strdup(key);
 		new->value = strdup(value);
-		new->next = ht->array[index + 1];
+		new->next = NULL;
 		ht->array[index] = new;
 		return (1);
 	}
