96575310564e80838adfcedd5d1c2b41ff80caf7
xica369 <761@holbertonschool.com>
2019-06-01 02:25:06 +0000

    T3: function that adds an element to the hash table

---
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -0,0 +1,49 @@
+#include "hash_tables.h"
+/**
+ *hash_table_set - function that adds an element to the hash table
+ *
+ *@ht: hash table you want to add or update the key/value
+ *@key: pointer to key
+ *@value: value associated with the key
+ *
+ *Return: 1 if it succeded else 0
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new, *head;
+	unsigned long int index;
+	const char *e = "";
+
+	if (ht == NULL || key == NULL || key == e || value == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	head = ht->array[0];
+	for (; head != NULL; head = head->next)
+	{
+		if (strcmp(key, head->key) == 0)
+		{
+			head->value = strdup(value);
+			return (1);
+		}
+	}
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
+
+	if (ht->array[index] == NULL)
+	{
+		new->key = strdup(key);
+		new->value = strdup(value);
+		new->next = ht->array[index + 1];
+		ht->array[index] = new;
+		return (1);
+	}
+	new->key = strdup(key);
+	new->value = strdup(value);
+	new->next = ht->array[index];
+	ht->array[0] = new;
+	return (1);
+}
