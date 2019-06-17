acaad406e877476606d26f205f97afd1036d04e2
xica369 <761@holbertonschool.com>
2019-06-17 02:08:20 +0000

    T3:  function that sorts an array of integers in ascending order using the Quick sort algorithm

---
+++ 0x1B-sorting_algorithms/3-quick_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * quick_sort - a function that sorts an array by quick sort
+ * @array: the array to be sorted
+ * @size: number of elements that the array has
+ */
+void quick_sort(int *array, size_t size)
+{
+	size_t i, j, start, end;
+
+
+}
