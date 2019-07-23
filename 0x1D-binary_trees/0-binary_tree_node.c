b4c9cda285fbf072163e66f63d8869d59aa8bf92
xica369 <761@holbertonschool.com>
2019-07-23 22:16:49 +0000

    T0: function that creates a binary tree node

--- 0x1D-binary_trees/0-binary_tree_node.c
+++ 0x1D-binary_trees/0-binary_tree_node.c
@@ -1,11 +1,25 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node -
+ * binary_tree_node - creates a binary tree node
  *
- * Return: 
+ * Return: a pointer to the new node, or NULL on failure
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
+	binary_tree_t *nodo;
+
+	nodo = malloc(sizeof(binary_tree_t));
+	if (nodo == NULL)
+	{
+		return NULL;
+	}
+
+	nodo->n = value;
+	nodo->left = NULL;
+	nodo->right = NULL;
+	nodo->parent = parent;
+
+	return (nodo);
 
 }
