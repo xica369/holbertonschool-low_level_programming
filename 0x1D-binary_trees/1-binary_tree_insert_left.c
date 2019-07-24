7502c5286d1b12164b84d70afaff24317e81fe51
xica369 <761@holbertonschool.com>
2019-07-24 01:31:50 +0000

    T1: update

--- 0x1D-binary_trees/1-binary_tree_insert_left.c
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -9,7 +9,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *nodo;
 
-	nodo = malloc(sizeof(binary_tree_t));
+	nodo = calloc(1, sizeof(binary_tree_t));
 	if (nodo == NULL)
 		return (NULL);
 	if (parent->left == NULL)
