b7a836de5412f2634aaa412c8c87962715837911
xica369 <761@holbertonschool.com>
2019-07-24 01:32:43 +0000

    T3: function that deletes an entire binary tree

--- 0x1D-binary_trees/3-binary_tree_delete.c
+++ 0x1D-binary_trees/3-binary_tree_delete.c
@@ -1,11 +1,20 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_delete -
- *
- * Return: 
+ * binary_tree_delete - function that deletes an entire binary tree
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
+	binary_tree_t *tempo, *left, *right;
 
+
+	if (tree == NULL)
+		return;
+	tempo = tree;
+	left = tree->left;
+	right = tree->right;
+	free(tempo);
+	if (left != NULL)
+		binary_tree_delete(left);
+	if (right != NULL)
+		binary_tree_delete(right);
 }
