a3394acbe57a47fbcd238281c44c253b63cf4b19
xica369 <761@holbertonschool.com>
2019-07-24 04:15:14 +0000

    T14: function that measures the balance factor of a binary tree

--- 0x1D-binary_trees/14-binary_tree_balance.c
+++ 0x1D-binary_trees/14-binary_tree_balance.c
@@ -1,11 +1,47 @@
 #include "binary_trees.h"
+size_t binary_tree_balance2(const binary_tree_t *tree);
 
 /**
- * binary_tree_balance -
+ * binary_tree_balance2 - measures the balance factor of a binary tree
  *
- * Return: 
+ *@tree: pointer to tree
+ *
+ *Return: Balance factor
  */
+
+size_t binary_tree_balance2(const binary_tree_t *tree)
+{
+	int cont1 = 0, cont2 = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left != NULL)
+	{
+
+		cont1 = binary_tree_balance2(tree->left);
+		cont1++;
+	}
+
+	if (tree->right != NULL)
+	{
+
+		cont2 = binary_tree_balance2(tree->right);
+		cont2++;
+	}
+
+	return (cont1 - cont2);
+}
+
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ *
+ *@tree: pointer to tree
+ *
+ *Return: Balance factor
+ */
+
 int binary_tree_balance(const binary_tree_t *tree)
 {
-
+	return (binary_tree_balance2(tree));
 }
