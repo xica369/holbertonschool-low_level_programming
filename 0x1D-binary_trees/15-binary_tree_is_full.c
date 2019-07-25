8d4263e8a1bd2955e6004b53df5fcb78d82886a9
xica369 <761@holbertonschool.com>
2019-07-25 03:58:14 +0000

    T15: function that checks if a binary tree is full

--- 0x1D-binary_trees/15-binary_tree_is_full.c
+++ 0x1D-binary_trees/15-binary_tree_is_full.c
@@ -1,6 +1,8 @@
 #include "binary_trees.h"
 /**
- * binary_tree_delete - binary_tree_preorder
+ * binary_tree_is_full - function that checks if a binary tree is full
+ *
+ *@tree: pointer to tree
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
