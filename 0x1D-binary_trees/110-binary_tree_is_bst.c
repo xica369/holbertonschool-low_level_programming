96941b93321a7c7e803bca6bcfc96ec8a5bb2ea3
xica369 <761@holbertonschool.com>
2019-07-25 04:36:53 +0000

    T110

--- 0x1D-binary_trees/110-binary_tree_is_bst.c
+++ 0x1D-binary_trees/110-binary_tree_is_bst.c
@@ -1,11 +1,26 @@
 #include "binary_trees.h"
 
+
+
 /**
- * binary_tree_is_bst -
+ * binary_tree_is_bst - checks if a binary tree is a valid Binary Search Tree
  *
- * Return: 
+ *@tree: pointer to the root node of the tree to traverse
+ *
+ *Return: 1 if tree is a valid BST, and 0 otherwise
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
+	if (tree != NULL)
+		return (0);
+	{
+		if (tree->left != NULL)
+			binary_tree_is_bst(tree->left);
+		
 
+		if (tree->right != NULL)
+			binary_tree_is_bst(tree->right);
+
+		if (
+	}
 }
