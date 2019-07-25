bbe23ef4cf572e46c9bd35cf73481cb3b3698cf9
xica369 <761@holbertonschool.com>
2019-07-25 01:53:47 +0000

    T2: add condiction about if the parent is NULL and betty

--- 0x1D-binary_trees/2-binary_tree_insert_right.c
+++ 0x1D-binary_trees/2-binary_tree_insert_right.c
@@ -3,11 +3,17 @@
 /**
  * binary_tree_insert_right - inserts a node as the right-child of another node
  *
+ *@parent: pointer to parent of the new node
+ *@value: value to save in n
+ *
  * Return: a pointer to the created node, or NULL
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *nodo;
+
+	if (parent == NULL)
+		return (NULL);
 
 	nodo = calloc(1, sizeof(binary_tree_t));
 	if (nodo == NULL)
