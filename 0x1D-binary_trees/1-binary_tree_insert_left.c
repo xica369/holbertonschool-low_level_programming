d9dfb7075cfb1b972231ccbdc97d9830bf479c8e
xica369 <761@holbertonschool.com>
2019-07-25 01:50:59 +0000

    T1: add condiction if parent is NULL

--- 0x1D-binary_trees/1-binary_tree_insert_left.c
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -12,6 +12,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *nodo;
 
+	if (parent == NULL)
+		return (NULL);
+
 	nodo = calloc(1, sizeof(binary_tree_t));
 	if (nodo == NULL)
 		return (NULL);
