7998f1e2b370f3bea710989d8c7580b99960039a
xica369 <761@holbertonschool.com>
2019-07-25 02:28:52 +0000

    T9: add condiction if tree is NULL

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -37,5 +37,8 @@ size_t binary_tree_height2(const binary_tree_t *tree)
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
+
 	return ((binary_tree_height2(tree)) - 1);
 }
