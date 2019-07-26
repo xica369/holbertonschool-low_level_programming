941398609d8ba4ad83025e1d28015fb67a2f2c51
xica369 <761@holbertonschool.com>
2019-07-26 02:53:24 +0000

    T26: function that builds a Binary Search Tree from an array

--- 0x1D-binary_trees/112-array_to_bst.c
+++ 0x1D-binary_trees/112-array_to_bst.c
@@ -7,5 +7,11 @@
  */
 bst_t *array_to_bst(int *array, size_t size)
 {
+	size_t cont = 0;
+	bst_t *root;
 
+	root = NULL;
+	for (cont = 0; cont < size; cont++)
+		bst_insert(&root, array[cont]);
+	return (root);
 }
