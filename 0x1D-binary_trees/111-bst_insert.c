f4fcc7125634635e2df024cc7928998639dc1593
xica369 <761@holbertonschool.com>
2019-07-26 20:19:47 +0000

    T25: betty

--- 0x1D-binary_trees/111-bst_insert.c
+++ 0x1D-binary_trees/111-bst_insert.c
@@ -1,9 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * bst_insert -
+ * bst_insert - inserts a value in a Binary Search Tree
  *
- * Return:
+ *@tree: double pointer to the root node of the BST to insert the value
+ *@value: value to store in the node to be inserted
+ *
+ * Return: a pointer to the created node, or NULL on failure
  */
 bst_t *bst_insert(bst_t **tree, int value)
 {
