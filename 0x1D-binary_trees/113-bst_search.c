c97fd0714d67f49fd94c0acb6ffd0161c7599a5a
xica369 <761@holbertonschool.com>
2019-07-26 03:06:21 +0000

    T27: function that searches for a value in a Binary Search Tree

--- 0x1D-binary_trees/113-bst_search.c
+++ 0x1D-binary_trees/113-bst_search.c
@@ -1,11 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * bst_search -
+ * bst_insert -
  *
- * Return: 
+ * Return:
  */
 bst_t *bst_search(const bst_t *tree, int value)
 {
-
+	if (tree == NULL)
+		return (NULL);
+	if (tree->n == value)
+		return ((void *)tree);
+	if (tree->n > value)
+		return (bst_search(tree->left, value));
+	if (tree->n < value)
+		return (bst_search(tree->right, value));
+	return (NULL);
 }
