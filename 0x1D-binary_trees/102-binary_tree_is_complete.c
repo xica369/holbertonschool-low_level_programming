ddf5c24596a234f3a5e8187efc7ed39f61c44fe3
xica369 <761@holbertonschool.com>
2019-07-27 03:55:04 +0000

    T21: function that checks if a binary tree is complete

--- 0x1D-binary_trees/102-binary_tree_is_complete.c
+++ 0x1D-binary_trees/102-binary_tree_is_complete.c
@@ -1,11 +1,15 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_complete -
+ * binary_tree_is_complete - checks if a binary tree is complete
  *
- * Return: 
+ *@tree: pointer to the root node of the tree to check
+ *
+ * Return: 1 if a binary tree is complete else 0
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-
+	if (tree = NULL)
+		return (0);
+	return (1);
 }
