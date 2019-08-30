7eaf15684fc265f5b5813a623e7f02f23ff73221
xica369 <761@holbertonschool.com>
2019-08-30 04:22:30 +0000

    T7: empty file

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,16 @@
+#include "search_algos.h"
+
+/**
+ *jump_search - searches for a value using the Jump search algorithm
+ *
+ *@array: a pointer to the first element of the array to search
+ *@size: the number of elements in array
+ *@value: the value to search
+ *
+ *Return: the first index where value is located
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	if (array == NULL)
+		return (-1);
+}
