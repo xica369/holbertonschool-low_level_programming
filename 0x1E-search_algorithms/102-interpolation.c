09d888f39274e10fdebbb0322683adcf00347b74
xica369 <761@holbertonschool.com>
2019-08-30 04:25:23 +0000

    T9: empty file

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,16 @@
+#include "search_algos.h"
+
+/**
+ *interpolation_search - searches a value with Interpolation search algorithm
+ *
+ *@array: a pointer to the first element of the array to search
+ *@size: the number of elements in array
+ *@value: the value to search
+ *
+ *Return: the first index where value is located
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	if (array == NULL)
+		return (-1);
+}
