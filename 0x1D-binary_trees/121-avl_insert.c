c916ac0182529698d789096695409d78199dcc1e
xica369 <761@holbertonschool.com>
2020-05-17 02:18:09 +0000

    update 121

--- 0x1D-binary_trees/121-avl_insert.c
+++ 0x1D-binary_trees/121-avl_insert.c
@@ -1,11 +1,14 @@
 #include "binary_trees.h"
 
 /**
- * array_to_avl -
+ * avl_insert - inserts a value in an AVL Tree
  *
- * Return: 
+ *@tree: double pointer to the root node of the AVL tree for inserting the value
+ *@value: value to store in the node to be inserted
+ *
+ * Return: a pointer to the created node, or NULL on failure
  */
-avl_t *array_to_avl(int *array, size_t size)
+avl_t *avl_insert(avl_t **tree, int value)
 {
-
+	avl_t *new_node = NULL;
 }
